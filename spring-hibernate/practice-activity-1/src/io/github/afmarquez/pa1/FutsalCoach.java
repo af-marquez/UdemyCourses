8bcaf938ef0ced42bcbe988f169f0ded5aebab2d
amarquez <79093871+af-marquez@users.noreply.github.com>
2023-01-09 15:53:49 +0000

    activity1

---
+++ spring-hibernate/practice-activity-1/src/io/github/afmarquez/pa1/FutsalCoach.java
@@ -0,0 +1,11 @@
+package io.github.afmarquez.pa1;
+public class FutsalCoach implements Coach {
+
+	@Override
+	public String getYourDailyWorkout() {
+		
+		return "Do your tactics review today";
+	}
+
+	
+}
